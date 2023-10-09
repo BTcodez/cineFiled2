5d9d8170a590bce375557d620e65df6a2f3aa5ee
Brian Tuohy <BTcodez22@gmail.com>
2023-10-09 16:53:13 -0500

    first commit
    2nd edition cineFiled app, new sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { CinefilesComponent } from './cinefiles/cinefiles.component';
+import { FilmComponent } from './shared/film/film.component';
+import { MyWatchlistComponent } from './watchlist/my-watchlist/my-watchlist.component';
+import { LibraryComponent } from './watchlist/library/library.component';
+import { SearchComponent } from './cinefiles/search/search.component';
+import { UserComponent } from './shared/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    WatchlistComponent,
+    CinefilesComponent,
+    FilmComponent,
+    MyWatchlistComponent,
+    LibraryComponent,
+    SearchComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cinefiles/cinefiles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinefiles',
+  templateUrl: './cinefiles.component.html',
+  styleUrls: ['./cinefiles.component.css']
+})
+export class CinefilesComponent {
+
+}
